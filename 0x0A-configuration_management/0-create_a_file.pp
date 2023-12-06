f3199ad72d3fc7795ac8d799fdcf5a074b31e7d7
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-06 17:17:24 +0000

    Create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # Ensure the presence of the file school
-node default {
-  file { '/tmp/school':
-    ensure  => file,
-    content => 'I love Puppet',
-    owner   => 'www-data',
-    group   => 'www-data',
-    mode    => '0744',
-  }
+file { '/tmp/school':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
