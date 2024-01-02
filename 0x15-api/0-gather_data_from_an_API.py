4afebf16507fd96ea461bd89977c9e46816912d2
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-02 15:30:56 +0000

    GET requet urllib.request

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Gather Data from an API"""
+
+if __name__ == '__main__':
+    import urllib.request
+    from sys import argv
+    import json
+
+    todo_url = 'https://jsonplaceholder.typicode.com/todos/?userId=' + argv[1]
+    todo_response = urllib.request.urlopen(todo_url)
+    user_url = 'https://jsonplaceholder.typicode.com/users/?id=' + argv[1]
+    user_response = urllib.request.urlopen(user_url)
+
+    todo = todo_response.read()
+    user = user_response.read()
+
+    todo = json.loads(todo)
+    user = json.loads(user)
+    done = 0
+    total = 0
+
+    for entry in todo:
+        if entry['completed'] is True:
+            done += 1
+        total += 1
+
+    print(f"Employee {user[0]['name']} is done with ({done}/{total}):")
+    for entry in todo:
+        if entry['completed'] is True:
+            print(f"\t {entry['title']}")
