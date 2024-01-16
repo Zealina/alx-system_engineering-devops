54906564261f7dac6db27ecc3cedfd8657729cfd
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-16 19:53:03 +0000

    Subreddit programming

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ZealinaHudson/1.0'}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
