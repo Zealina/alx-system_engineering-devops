d1762e56f1f39e9f64ff56915b0813ad7c244fe8
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-16 20:29:55 +0000

    return subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""Total Number of Subscribers"""
+""" Function that queries the Reddit API """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'ZealinaHudson/1.0'}
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
-        return (response.json().get('data').get('subscribers'))
-    return 0
+        return (response.json().get("data").get("subscribers"))
+    return (0)
