1c122b1c0c38f5f0ed0eb9e90f8c97ec230af301
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-16 20:19:01 +0000

    Return Subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,6 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'ZealinaHudson/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
